e79651ec30035ecf9f8e53153c60967ba4a84958
Heidy <heidyguevara23@hotmail.com>
2024-05-12 18:06:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        public delegate void ShowMoney(double amount);
+
+        static void Main(string[] args)
+        {
+            ShowMoney showMoney = ShowDollars;
+            showMoney(100.50);
+
+            showMoney = ShowEuros;
+            showMoney(100.50);
+        }
+
+        public static void ShowDollars(double amount)
+        {
+            Console.WriteLine($"{amount.ToString("0.00")} $");
+        }
+
+        public static void ShowEuros(double amount)
+        {
+            Console.WriteLine($"{amount.ToString("0.00")} €");
+        }
+    }
+}
